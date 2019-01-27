922c0301e28354239cac5c5ba8916b8a588136f5
Corey Alexander <coreyja@gmail.com>
2019-01-27 17:06:44 -0500

    Add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user_1 = FactoryBot.create(:user, name: Faker::HarryPotter.character, email: Faker::Internet.unique.email)
+user_2 = FactoryBot.create(:user, name: Faker::HowIMetYourMother.character, email: Faker::Internet.unique.email)
+user_3 = FactoryBot.create(:user, name: Faker::HarryPotter.character, email: Faker::Internet.unique.email)
+user_4 = FactoryBot.create(:user, name: Faker::HarryPotter.character, email: Faker::Internet.unique.email)
+
+user_1_post_1 = FactoryBot.create(:post, user: user_1, title: Faker::HarryPotter.spell, content: Faker::HarryPotter.quote)
+user_1_post_2 = FactoryBot.create(:post, user: user_1, title: Faker::HarryPotter.spell, content: Faker::HarryPotter.quote)
+user_1_post_3 = FactoryBot.create(:post, user: user_1, title: Faker::HarryPotter.spell, content: Faker::HarryPotter.quote)
+
+user_2_post_1 = FactoryBot.create(:post, user: user_2, title: Faker::HowIMetYourMother.high_five, content: Faker::HowIMetYourMother.quote)
+user_2_post_2 = FactoryBot.create(:post, user: user_2, title: Faker::HowIMetYourMother.high_five, content: Faker::HowIMetYourMother.quote)
+user_2_post_3 = FactoryBot.create(:post, user: user_2, title: Faker::HowIMetYourMother.high_five, content: Faker::HowIMetYourMother.quote)
+user_2_post_4 = FactoryBot.create(:post, user: user_2, title: Faker::HowIMetYourMother.high_five, content: Faker::HowIMetYourMother.quote)
+
+FactoryBot.create(:comment, post: user_1_post_1, user: user_3)
+FactoryBot.create(:comment, post: user_2_post_1, user: user_3)
+FactoryBot.create(:comment, post: user_2_post_3, user: user_3)
+
+FactoryBot.create(:comment, post: user_2_post_4, user: user_1)
+FactoryBot.create(:comment, post: user_2_post_3, user: user_1)
